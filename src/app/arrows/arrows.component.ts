bed13e7399668bdc77a4e76a4da610cab5eaa26f
Daniela Olbrich <innoshea@googlemail.com>
2024-08-03 16:38:25 +0200

    added project

---
+++ src/app/arrows/arrows.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-arrows',
+  standalone: true,
+  imports: [],
+  templateUrl: './arrows.component.html',
+  styleUrl: './arrows.component.scss'
+})
+export class ArrowsComponent {
+
+}
